697fb850dc5dfbebce09aac579c97665ef9cbd36
Juiceman78 <vincentz78@nycstudents.net>
2025-03-20 10:16:19 -0400

    KASSSSSSSSSSSSSSA

--- Flash.py
+++ Flash.py
@@ -1,1 +1,30 @@
-class user():
+import json
+
+into = input("Are you a Student or Teacher: ")
+
+class Student:
+    def answer_cards:
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if info == "Student":
+    Student.answer_cards
+elif info == "Teacher":
+    Teacher.make_cards
